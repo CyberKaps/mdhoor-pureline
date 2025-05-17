94f339cd12f7e4d346916682009ca8879d11087c
Kalpesh Borde <bordekalpesh@gmail.com>
2025-05-17 13:14:51 +0530

    made groundnut section responsive

--- src/components/GroundnutOil.jsx
+++ src/components/GroundnutOil.jsx
@@ -3,12 +3,12 @@ import appImage from "../assets/madhoorPureline.png";
 
 const GroundnutOil = () => {
   return (
-    <section className="bg-[#fff7f6] py-2 ">
-      <h1 className='raleway-heading text-4xl md:text-7xl lg:text-8xl mt-4 text-[#9BEC00] text-center'>
+    <section className="bg-[#fff7f6] py-2">
+      <h1 className="raleway-heading text-4xl md:text-7xl lg:text-8xl mt-4 text-[#9BEC00] text-center">
         Groundnut Oil
       </h1>
       <div className="flex justify-around">
-        <div className="max-w-7xl mx-auto px-6 flex items-center gap-x-20">
+        <div className="max-w-7xl mx-auto px-6 flex flex-col lg:flex-row items-center gap-x-20 gap-y-10">
           {/* Text Section */}
           <div className="lg:w-1/2 text-center lg:text-left">
             <h2 className="text-3xl md:text-4xl font-bold text-gray-900 leading-snug mb-6">
@@ -22,11 +22,11 @@ const GroundnutOil = () => {
           </div>
 
           {/* Image Section */}
-          <div className="lg:w-1/2 relative">
+          <div className="w-full lg:w-1/2 relative flex justify-center">
             <img
               src={appImage}
-              alt="SweeBuzz App"
-              className="w-full max-w-md mx-auto drop-shadow-xl"
+              alt="Groundnut oil"
+              className="w-full max-w-md drop-shadow-xl"
             />
             <div className="absolute bg-[#FF6C4A] rounded-full w-72 h-72 -z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-20"></div>
           </div>
